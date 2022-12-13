f247e77acd1e3e740f6b31d8d331758056efa564
Gaurav Jaiswal <gaurav.rcet@gmail.com>
2022-12-13 14:54:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
+// Unit of multiple charts of react google charts in this app
+
 import "./styles.css";
 
-import { Chart } from "react-google-charts";
-
-const data = [
-  ["Year wise graph", "sales(in Rs)", "expenses(in Rs)", "profit(in Rs)"],
-  ["2016", 1000, 400, 200],
-  ["2017", 1100, 600, 250],
-  ["2018", 900, 300, 300],
-  ["2019", 1200, 1000, 200],
-  ["2020", 800, 100, 400],
-  ["2021", 1300, 500, 400]
-];
+import Pye from "./Pye";
+import Colch from "./Colch";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Google Charts in React</h1>
-      <h2> unit mini projects </h2>
-
-      <Chart
-        chartType="Bar"
-        data={data}
-        width="80%"
-        height="400px"
-        legendToggle
-      />
+      <h1 className>Google Charts in React</h1>
+      <h2> unit of mini projects </h2>
+      <div>
+        <Pye />
+        <Colch />
+      </div>
     </div>
   );
 }
